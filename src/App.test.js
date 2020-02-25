d8d0b455924cb1c27b656d6449f09f8871d3298c
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-25 11:18:01 +0000

    First test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {ImageUrls} from "./data";
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('count number of urls', () => {
+  //arrange
+  const imageUrls = ImageUrls;
+  const arrayLength = imageUrls.length;
+  //act
+  
+  //assert
+  expect(arrayLength).toBe(44);
+  
+  
 });
