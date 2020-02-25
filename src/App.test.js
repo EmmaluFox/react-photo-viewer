dc88801cf4e9985bc276c7bec54891a3eff02733
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-25 12:50:27 +0000

    Second test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
 import App from './App';
 import {ImageUrls} from "./data";
 import {PhotoViewer} from "./PhotoViewer/PhotoViewer";
 import TestRenderer from 'react-test-renderer';
+import {Thumbnail} from "./Thumbnail/Thumbnail";
+import {wait} from "@testing-library/dom";
+import * as thumbnail from "@testing-library/react/dist/@testing-library/react.pure.umd";
+import {PhotoSelector} from "./PhotoSelector/PhotoSelector";
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
@@ -13,8 +17,7 @@ test('renders learn react link', () => {
 
 test('count number of urls', () => {
   //arrange
-  const imageUrls = ImageUrls;
-  const arrayLength = imageUrls.length;
+  const arrayLength = ImageUrls.length;
   //act
   
   //assert
@@ -28,3 +31,28 @@ describe("PhotoViewer", () => {
   })
 
 });
+
+describe("Thumbnail", () => {
+  
+  it("Should select thumbnail on click", async () => {
+    const {getByClassName: getByClassName} = render(<PhotoSelector selectedUrl={"https://i.picsum.photos/id/602/1600/900.jpg"} setSelectedUrl={"https://i.picsum.photos/id/602/1600/900.jpg"}/>);
+    const unSelectedThumbnail = getByClassName("thumbnail-image");
+    const selectedThumbnail = getByClassName("thumbnail-image selected");
+    
+    const photoSelector = render(<PhotoSelector selectedUrl={"https://i.picsum.photos/id/602/1600/900.jpg"} setSelectedUrl={"https://i.picsum.photos/id/602/1600/900.jpg"}/>);
+    await wait(()=> );
+
+    fireEvent.click(photoSelector);
+    
+    await wait(()=> thumbnailSelectedTrue);
+    
+    
+    
+  })
+  
+  
+
+});
+
+
+
